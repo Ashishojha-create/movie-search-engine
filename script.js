79ab3435b81db532d0aece31508508f1dde79059
Ashish Kumar Ojha <kumarojhaashish84@gmail.comm>
2026-06-30 21:55:35 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const movies = [
+{
+title:"Inception",
+year:"2010",
+genre:"Sci-Fi",
+rating:"8.8",
+poster:"images/inception.jpg",
+plot:"A thief steals information by entering dreams."
+},
+{
+title:"Interstellar",
+year:"2014",
+genre:"Adventure",
+rating:"8.7",
+poster:"images/interstellar.jpg",
+plot:"A team travels through a wormhole to save humanity."
+},
+{
+title:"Avatar",
+year:"2009",
+genre:"Action",
+rating:"7.9",
+poster:"images/avatar.jpg",
+plot:"A marine becomes involved in the world of Pandora."
+},
+{
+title:"The Dark Knight",
+year:"2008",
+genre:"Action",
+rating:"9.0",
+poster:"images/darkknight.jpg",
+plot:"Batman battles the Joker."
+},
+{
+title:"Titanic",
+year:"1997",
+genre:"Romance",
+rating:"7.9",
+poster:"images/titanic.jpg",
+plot:"A love story aboard the Titanic."
+}
+];
